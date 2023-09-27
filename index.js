cf1fc745b1688173e76e8654a0504bf966adc453
sleeping <umaruchan2k1@gmail.com>
2023-09-27 22:27:41 +0700

    Comment

---
+++ index.js
@@ -0,0 +1,19 @@
+let devices = {
+  Android: "https://apps.apple.com/vn/app/ivirse-app/id6446141074",
+  IOS: "https://play.google.com/store/apps/details?id=com.ivirse.wallet",
+  others: "other device",
+};
+
+let deviceType = () => {
+  if (navigator.userAgent.match(/Android/i)) {
+    return "Android";
+  } else if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+    return "IOS";
+  } else {
+    return "others";
+  }
+};
+
+window.onload = () => {
+  window.location.href = devices[deviceType()];
+};
